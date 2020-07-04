363a9aa8d8dd385c73c146a00843ee227aaeb421
robin.son <robin.son@kakaocorp.com>
2020-07-04 17:37:09 +0900

    Initial commit

---
+++ src/main/java/com/example/completablefuture2/Completablefuture2Application.java
@@ -0,0 +1,13 @@
+package com.example.completablefuture2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Completablefuture2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Completablefuture2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/completablefuture2/Completablefuture2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.completablefuture2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Completablefuture2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
